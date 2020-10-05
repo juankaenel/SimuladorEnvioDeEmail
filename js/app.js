acf24ce33b29ea4e1b3c181df89197e39f43bae2
Juan <kaenel10@gmail.com>
2020-10-05 17:48:42 -0300

    Primeros pasos con el proyecto

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar');
+
+
+
+eventListeners();
+function eventListeners(){
+document.addEventListener('DOMContentLoaded', iniciarApp);
+}
+
+//Funciones
+function iniciarApp(){
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+    console.log('Iniciando...');
+}
