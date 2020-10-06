f22e8c66c1da133b794a3813fc8c900c561dedb3
Juan <kaenel10@gmail.com>
2020-10-06 16:34:47 -0300

    Limpiar los mensajes de error

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const btnEnviar = document.querySelector('#enviar');
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
-
+const formulario = document.querySelector('#enviar-mail');
 
 
 eventListeners();
@@ -34,5 +34,22 @@ function validarFormulario(e){
     else{
         //e.target.style.borderBottomColor = 'red';
         e.target.classList.add('border','border-red-500'); //le doy clases de tailwind cuando no hay nada en el input
+        
+        mostrarError();
     }
+}
+
+function mostrarError(){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border','border-red-500','background-red-100','text-red-500','p-3','mt-2','text-center','error'); //la clase error me permitirá hacer el control de cuantos campos tienen error y con eso controlo que no se repita mas de una vez el msj de error
+
+    const errores = document.querySelectorAll('.error'); //con queryselectorall puedo acceder al lenght, y verificar si hay mas de 1 error, con queryselector solo no
+
+    if(errores.length ===0){
+        formulario.appendChild(mensajeError);
+        //formulario.insertBefore(mensajeError, document.querySelector('.mb-10'));
+    }
+
+    
 }
